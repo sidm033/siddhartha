6e141b2555fa03ac42ba50d21fee78cd61021373
S-iddhartham <siddhartha@wrctpl.com>
2026-06-18 19:03:56 +0530

    Deploy: fix email, GitHub link, click-through

---
+++ _next/static/chunks/app/layout-41763656501fe828.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3673:()=>{},8035:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3673,23))}},e=>{e.O(0,[978,441,255,358],()=>e(e.s=8035)),_N_E=e.O()}]);
